d6ac5b0742d413fb66f19a5b1f68ee62d5221b0f
Lily <bro21019@byui.edu>
2022-06-10 16:37:38 -0600

    com

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Counter from './Counter.js';
+import Login from './Login.js';
 import SettingsScreen from './SettingsScreen.js';
 import Home from './Home.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
@@ -13,6 +14,9 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  if (userLoggedIn){
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -52,11 +56,15 @@ export default function App() {
         />
       </Tab.Navigator>
     </NavigationContainer>
+  );      
+} else {
+  return(
+    <Login/>
   );
+}
 
 
 }
-
 
 const styles = StyleSheet.create({
   
